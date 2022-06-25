1e3e40ac749bff725ee7c7e18c370f1bf3d161b5
Evelyn <evc776@gmail.com>
2022-06-25 09:51:17 +0100

    meow 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Meow");
         System.out.println("Meow again!");
+        System.out.println("Meow!");
     }
 }
